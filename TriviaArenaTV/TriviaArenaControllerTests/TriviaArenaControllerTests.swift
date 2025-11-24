e8646d024a46965715c9f54fe002dc071af98838
Sadeepa Piyumal <129213685+sadeepapiyumal@users.noreply.github.com>
2025-11-24 21:46:10 +0530

    Update TriviaArenaControllerTests.swift

--- TriviaArenaTV/TriviaArenaControllerTests/TriviaArenaControllerTests.swift
+++ TriviaArenaTV/TriviaArenaControllerTests/TriviaArenaControllerTests.swift
@@ -1,9 +1,5 @@
-//
+
 //  TriviaArenaControllerTests.swift
-//  TriviaArenaControllerTests
-//
-//  Created by IM Student on 2025-11-16.
-//
 
 import Testing
 @testable import TriviaArenaController
